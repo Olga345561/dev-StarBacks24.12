3d22a13079697cf6be68216dec4e96cf2ef0a6fe
olga345561 <buszachka@gmail.com>
2025-03-22 17:36:40 +0200

    feat: creat header

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,8 @@
+  const burger = document.querySelector('.btn-burger');
+  const mobileContainer = document.querySelector('.mobile-container')
+
+  burger.addEventListener('click', ()=> {
+    burger.classList.toggle('active')
+    mobileContainer.classList.toggle('show')
+  })
   
